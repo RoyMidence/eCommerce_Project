4bb584cf35692df83910426016a8f6ebc457de49
RoyMidence <RoyMidence@users.noreply.github.com>
2022-07-09 15:22:26 -0400

    Just experimenting in Kotlin. Don't know the language so just having fun for now

--- app/src/main/java/com/example/roymart/MainActivity.kt
+++ app/src/main/java/com/example/roymart/MainActivity.kt
@@ -2,12 +2,23 @@ package com.example.roymart
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var buttonChangeText: Button
+    private lateinit var textViewWord: TextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        buttonChangeText = findViewById(R.id.buttonChangeText)
+        textViewWord = findViewById(R.id.textViewWord)
+
+        buttonChangeText.setOnClickListener {
+            textViewWord.text = "GAAAAAAA"
+        }
     }
 
 }
